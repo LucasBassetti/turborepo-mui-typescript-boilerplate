f271f9379be79a192465cea8310bcedc9a3dc2d8
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2023-01-31 00:48:25 -0300

    chore: remove storybook-addon-material-ui5

--- apps/web/.storybook/main.js
+++ apps/web/.storybook/main.js
@@ -10,7 +10,7 @@ module.exports = {
     }
   },
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials', 'storybook-addon-material-ui5'],
+  addons: ['@storybook/addon-essentials'],
   babel: async (options) => ({
     ...options,
     plugins: [...options.plugins, require.resolve('@babel/plugin-transform-react-jsx')],
